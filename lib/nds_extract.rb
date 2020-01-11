1441366ab9f367e4bb6cccbddca27eadc21ab899
Kelly Becker <kelly.lynn.becker@gmail.com>
2020-01-11 17:23:39 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -37,7 +37,8 @@ def list_of_directors(source)
 end
 
 def total_gross(source)
-  totals_by_director
+  totals_by_director = directors_totals(source)
+  names = list_of_directors(source)
   # Write this implementation
   #
   # Should use methods:
